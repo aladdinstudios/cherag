c6e5673425b3429765e181bbe41f4f059e6ed3d6
itsaladin <alauddin12340@gmail.com>
2022-08-21 21:13:30 +0600

    feat: setup rn-vector icon

--- next.config.js
+++ next.config.js
@@ -7,6 +7,21 @@ const nextConfig = {
       unoptimized: true,
     },
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      "react-native$": "react-native-web",
+    };
+    config.resolve.extensions = [
+      ".web.js",
+      ".web.jsx",
+      ".web.ts",
+      ".web.tsx",
+      ...config.resolve.extensions,
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
