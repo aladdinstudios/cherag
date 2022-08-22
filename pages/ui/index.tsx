ea3a76bb8887a3a51e8f859d1b5123fe0d6476b5
itsaladin <alauddin12340@gmail.com>
2022-08-23 02:57:06 +0600

    Design structure of /UI dir index page

--- pages/ui/index.tsx
+++ pages/ui/index.tsx
@@ -4,17 +4,32 @@ import { NextPageWithLayout } from "../_app";
 
 const Ui: NextPageWithLayout = () => {
   return (
-    <Div style={{ width: "80%" }}>
-      <Text>
-        What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
-        and typesetting industry. Lorem Ipsum has been the industry's standard
-        dummy text ever since the 1500s, when an unknown printer took a galley
-        of type and scrambled it to make a type specimen book. It has survived
-        not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with
-        the release of Letraset sheets containing Lorem Ipsum passages, and more
-        recently with desktop publishing software like Aldus PageMaker including
-        versions of Lorem Ipsum.
+    <Div style={{ width: "25%", marginTop: 50 }}>
+      <Text fontSize="4xl">
+        Cross-platform Cherag UI design for React Native.
+      </Text>
+      <Text style={{ marginVertical: 30, textAlign: "justify" }}>
+        Leverage agile frameworks to provide a robust synopsis for high level
+        overviews. Iterative approaches to corporate strategy foster
+        collaborative thinking to further the overall value proposition.
+        Organically grow the holistic world view of disruptive innovation via
+        workplace diversity and empowerment. Bring to the table win-win survival
+        strategies to ensure proactive domination. At the end of the day, going
+        forward, a new normal that has evolved from generation X is on the
+        runway heading towards a streamlined cloud solution. User generated
+        content in real-time will have multiple touchpoints for offshoring.
+      </Text>
+
+      <Text style={{ textAlign: "justify" }}>
+        Leverage agile frameworks to provide a robust synopsis for high level
+        overviews. Iterative approaches to corporate strategy foster
+        collaborative thinking to further the overall value proposition.
+        Organically grow the holistic world view of disruptive innovation via
+        workplace diversity and empowerment. Bring to the table win-win survival
+        strategies to ensure proactive domination. At the end of the day, going
+        forward, a new normal that has evolved from generation X is on the
+        runway heading towards a streamlined cloud solution. User generated
+        content in real-time will have multiple touchpoints for offshoring.
       </Text>
     </Div>
   );
