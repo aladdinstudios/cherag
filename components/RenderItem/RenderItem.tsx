fbbf7b894894d930a6cd71b485cdba0d6ef4f017
itsaladin <alauddin12340@gmail.com>
2022-08-23 17:29:02 +0600

    Make dynamic RenderItem comp.

--- components/RenderItem/RenderItem.tsx
+++ components/RenderItem/RenderItem.tsx
@@ -1,91 +1,121 @@
 import { Alert, COLORS, Div, Divider, Text } from "cherag-ui";
-import React from "react";
+import React, { Children, FC, ReactNode } from "react";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import { ProjectColor } from "../../theme";
 
-export const RenderItem = () => {
-  const importCodeString = `import {Alert} from 'cherag-ui'`;
-  const useCaseCodeString = `<Alert status="warning" title="Item deleted !!!" />`;
+export type ObjProps = {
+  obj1: {
+    name: string;
+    type: string;
+    des: string;
+  };
+  obj2: {
+    name: string;
+    type: string;
+    des: string;
+  };
+};
+
+export type RenderItemProps = {
+  componentName: string;
+  importCodeString: string;
+  useCaseCodeString: string;
+  children: ReactNode;
+  obj: ObjProps;
+};
+
+export const RenderItem: FC<RenderItemProps> = ({
+  componentName,
+  importCodeString,
+  useCaseCodeString,
+  children,
+  obj,
+}) => {
   return (
-    <Div>
-      <Div style={{ marginTop: 80, marginLeft: 80, height: "auto" }}>
-        <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 20 }}>
-          Alert
-        </Text>
-        <Text style={{ marginBottom: 20 }}>
-          Component for displaying messages, notifications, or other application
-          state.
-        </Text>
+    <Div style={{ marginTop: 80, marginLeft: 80, height: "auto" }}>
+      <Text style={{ fontSize: 30, fontWeight: "bold", marginBottom: 20 }}>
+        {componentName}
+      </Text>
+      <Text style={{ marginBottom: 20 }}>
+        Component for displaying messages, notifications, or other application
+        state.
+      </Text>
 
-        <SyntaxHighlighter language="tsx" style={docco}>
-          {importCodeString}
-        </SyntaxHighlighter>
-        <Text style={{ fontSize: 30, fontWeight: "bold", marginVertical: 20 }}>
-          Usage
-        </Text>
-        <SyntaxHighlighter language="tsx" style={docco}>
-          {useCaseCodeString}
-        </SyntaxHighlighter>
+      <SyntaxHighlighter language="tsx" style={docco}>
+        {importCodeString}
+      </SyntaxHighlighter>
+      <Text style={{ fontSize: 30, fontWeight: "bold", marginVertical: 20 }}>
+        Usage
+      </Text>
+      <SyntaxHighlighter language="tsx" style={docco}>
+        {useCaseCodeString}
+      </SyntaxHighlighter>
 
-        <Div
-          style={{
-            marginTop: 30,
-            backgroundColor: COLORS.lightGray5,
-            width: "100%",
-            height: 200,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Alert status="success" title="Product add Successfully" />
-        </Div>
-
-        <Text style={{ fontSize: 30, fontWeight: "bold", marginVertical: 20 }}>
-          Props
-        </Text>
-        <Div style={{ flexDirection: "row" }}>
-          <Text style={{ fontWeight: "bold", width: 100 }}>Name</Text>
-          <Text style={{ fontWeight: "bold", width: 100 }}>Type</Text>
-          <Text style={{ fontWeight: "bold" }}>Description</Text>
-        </Div>
-        <Divider
-          thickness={1}
-          color={ProjectColor.border}
-          style={{
-            marginVertical: 10,
-          }}
-        />
-        <Div
-          style={{
-            flexDirection: "row",
-          }}
-        >
-          <Text style={{ width: 100 }}>status</Text>
-          <Text style={{ width: 100 }}>String</Text>
-          <Text>Type of component, Component colors.</Text>
-        </Div>
-        <Divider
-          thickness={1}
-          color={ProjectColor.border}
-          style={{
-            marginVertical: 10,
-          }}
-        />
-        <Div style={{ flexDirection: "row" }}>
-          <Text style={{ width: 100 }}>title</Text>
-          <Text style={{ width: 100 }}>String</Text>
-          <Text>Title of the Alert component.</Text>
-        </Div>
-
-        <Divider
-          thickness={1}
-          color={ProjectColor.border}
-          style={{
-            marginVertical: 10,
-          }}
-        />
+      <Div
+        style={{
+          marginTop: 30,
+          backgroundColor: COLORS.lightGray5,
+          width: "100%",
+          height: 200,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {children}
       </Div>
+
+      <Text style={{ fontSize: 30, fontWeight: "bold", marginVertical: 20 }}>
+        Props
+      </Text>
+      <Div style={{ flexDirection: "row" }}>
+        <Text style={{ fontWeight: "bold", width: 100 }}>Name</Text>
+        <Text style={{ fontWeight: "bold", width: 100 }}>Type</Text>
+        <Text style={{ fontWeight: "bold" }}>Description</Text>
+      </Div>
+      <Divider
+        thickness={1}
+        color={ProjectColor.border}
+        style={{
+          marginVertical: 10,
+        }}
+      />
+      <Div
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Text style={{ width: 100 }}>{obj.obj1.name}</Text>
+        <Text style={{ width: 100 }}>{obj.obj1.type}</Text>
+        <Text style={{ width: 500, textAlign: "justify" }}>
+          Type of component, Component colors.Type of component, Component
+          colors.Type of component, Component colors.Type of component,
+          Component colors.Type of component, Component colors. Type of
+          component, Component colors.Type of component, Component colors.Type
+          of component, Component colors.Type of component, Component
+          colors.Type of component, Component colors.
+        </Text>
+      </Div>
+      <Divider
+        thickness={1}
+        color={ProjectColor.border}
+        style={{
+          marginVertical: 10,
+        }}
+      />
+      <Div style={{ flexDirection: "row" }}>
+        <Text style={{ width: 100 }}>{obj.obj2.name}</Text>
+        <Text style={{ width: 100 }}>{obj.obj2.type}</Text>
+        <Text>{obj.obj2.des}</Text>
+      </Div>
+
+      <Divider
+        thickness={1}
+        color={ProjectColor.border}
+        style={{
+          marginVertical: 10,
+        }}
+      />
     </Div>
   );
 };
--- pages/ui/Alert.tsx
+++ pages/ui/Alert.tsx
@@ -1,16 +1,32 @@
-import { Alert, COLORS, Div, Divider, Text } from "cherag-ui";
-import Layout from "../../components/Layout";
-import { NextPageWithLayout } from "../_app";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { Alert } from "cherag-ui";
 import { ScrollView } from "react-native";
-import { ProjectColor } from "../../theme";
+import Layout from "../../components/Layout";
 import { RenderItem } from "../../components/RenderItem";
+import { NextPageWithLayout } from "../_app";
 
 const About: NextPageWithLayout = () => {
+  const obj = {
+    obj1: {
+      name: "name",
+      type: "string",
+      des: "Type of component, Component colors.Type of component, Component",
+    },
+    obj2: {
+      name: "title",
+      type: "string",
+      des: "Title of the Alert component",
+    },
+  };
   return (
     <ScrollView showsHorizontalScrollIndicator={false}>
-      <RenderItem />
+      <RenderItem
+        componentName={"Alart"}
+        importCodeString={`import {Alert} from 'cherag-ui'`}
+        useCaseCodeString={`<Alert status="warning" title="Item deleted !!!" />`}
+        obj={obj}
+      >
+        <Alert status="success" title="Product add Successfully" />
+      </RenderItem>
     </ScrollView>
   );
 };
