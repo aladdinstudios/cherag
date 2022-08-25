aa3bdcc9c7f50b16047bf8057a3d05e019d484aa
Alauddin Ahmed <alauddin12340@gmail.com>
2022-08-25 12:42:51 +0600

    Fix Checkbox design issue

--- pages/ui/Checkbox.tsx
+++ pages/ui/Checkbox.tsx
@@ -13,61 +13,23 @@ export interface UsecaseArayObjProps {
 
 const CheckboxComponent: NextPageWithLayout = () => {
   const [checkboxes, setCheckboxes] = useState<CheckboxValue[]>([]);
+  let __componentDescription =
+    "Checkboxes allow the selection of multiple options from various childred.";
   let __propertics: ProperticsProps[] = [
     {
-      name: "title",
-      type: "string",
-      des: "Title of card.",
-    },
-    {
-      name: "subTitle",
-      type: "string",
-      des: "Subtitle of card.",
-    },
-    {
-      name: "footerText",
-      type: "string",
-      des: "Footer text of card.",
-    },
-    {
-      name: "category",
-      type: "string",
-      des: "Name of the category.",
-    },
-    {
-      name: "w",
-      type: "number",
-      des: "Image with of card.",
-    },
-    {
-      name: "h",
-      type: "number",
-      des: "Image height of card.",
-    },
-    {
-      name: "categoryBgColor",
-      type: "string",
-      des: "Category background color of card.",
+      name: "values",
+      type: "string[]",
+      des: "Selected items of checkbox.",
     },
     {
       name: "children",
-      type: "React.ReactNode | string",
-      des: "Paragraph of card.",
+      type: "React.ReactNode",
+      des: "Child elements of checkbox.",
     },
     {
-      name: "titleStyle",
-      type: "StyleProp<ViewStyle>",
-      des: "Title style of card.",
-    },
-    {
-      name: "subTitleStyle",
-      type: "StyleProp<ViewStyle>",
-      des: "Subtitle style of card.",
-    },
-    {
-      name: "paragraphStyle",
-      type: "StyleProp<ViewStyle>",
-      des: "Paragraph style of card.",
+      name: "onChange",
+      type: "(values: any) => any",
+      des: "The callback return when any children Checkbox is checked or unchecked..",
     },
   ];
 
@@ -93,6 +55,7 @@ const CheckboxComponent: NextPageWithLayout = () => {
     <ScrollView showsHorizontalScrollIndicator={false}>
       <RenderAlertItem
         componentName={"Checkbox"}
+        componentDescription={__componentDescription}
         importCodeString={__importCodeString}
         usecaseCodeStringArr={__useCaseCodeStringArr}
         propertics={__propertics}
