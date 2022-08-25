1465d677bb2418489d98e33ee18eac20395437d9
Alauddin Ahmed <alauddin12340@gmail.com>
2022-08-25 23:01:23 +0600

    Update Card component

--- pages/ui/Card.tsx
+++ pages/ui/Card.tsx
@@ -9,6 +9,36 @@ const CardComponent: NextPageWithLayout = () => {
   let __componentName = "Card";
   let __componentDescription =
     "Card component is a structure of material that provide as an entry point to more detailed information.";
+  let __importCodeString = `import { Card } from 'cherag-ui'`;
+  let __useCaseCodeStringArr: UsecaseArayObjProps[] = [
+    {
+      name: "",
+      code: `
+    <Card
+      w={250}
+      style={{
+        width: 250,
+        height: 380,
+      }}
+      title="The Garden City"
+      subTitle="The Silicon Valley of BD"
+      category="Photo"
+      footerText="6 mins ago"
+      footerStyle={{
+        width: 80,
+        fontSize: 14,
+      }}
+      paragraphStyle={{ fontSize: 14, color: COLORS.blackish }}
+      subTitleStyle={{ fontSize: 12 }}
+      titleStyle={{ fontSize: 22, color: COLORS.blackish }}
+    >
+      Bengaluru also called Bangalore is the center of BD high-tech
+      industry. The city is also known for its parks and nightlife Bengaluru
+      also called Bangalore is the center of BD high-tech industry. The city
+      is also known for its parks and nightlife
+   </Card>`,
+    },
+  ];
   let __propertics: ProperticsProps[] = [
     {
       name: "title",
@@ -64,36 +94,6 @@ const CardComponent: NextPageWithLayout = () => {
       name: "paragraphStyle",
       type: "StyleProp<ViewStyle>",
       des: "Paragraph style of card.",
-    },
-  ];
-  let __importCodeString = `import { Card } from 'cherag-ui'`;
-  let __useCaseCodeStringArr: UsecaseArayObjProps[] = [
-    {
-      name: "",
-      code: `
-    <Card
-      w={250}
-      style={{
-        width: 250,
-        height: 380,
-      }}
-      title="The Garden City"
-      subTitle="The Silicon Valley of BD"
-      category="Photo"
-      footerText="6 mins ago"
-      footerStyle={{
-        width: 80,
-        fontSize: 14,
-      }}
-      paragraphStyle={{ fontSize: 14, color: COLORS.blackish }}
-      subTitleStyle={{ fontSize: 12 }}
-      titleStyle={{ fontSize: 22, color: COLORS.blackish }}
-    >
-      Bengaluru also called Bangalore is the center of BD high-tech
-      industry. The city is also known for its parks and nightlife Bengaluru
-      also called Bangalore is the center of BD high-tech industry. The city
-      is also known for its parks and nightlife
-   </Card>`,
     },
   ];
 
