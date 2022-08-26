79e7cf669113550d845a5ebd84da9eeab82c59d7
Alauddin Ahmed <alauddin12340@gmail.com>
2022-08-27 02:09:12 +0600

    Change SidebarProps name

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,9 +1,9 @@
 import { Button, COLORS } from "cherag-ui";
 import Link from "next/link";
 import { FC } from "react";
-import { Props } from "./types";
+import { SidebarProps } from "./types";
 
-const Sidebar: FC<Props> = ({ children }) => {
+const Sidebar: FC<SidebarProps> = ({ children }) => {
   return (
     <Link href={`/ui/${children}`} passHref>
       <Button width={150} bg={COLORS.white} fontSize={18} color={COLORS.black}>
