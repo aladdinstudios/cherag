7a16e602dc44cef1dab113157f26d517e3b5a285
Alauddin Ahmed <alauddin12340@gmail.com>
2022-08-29 00:18:07 +0600

    Create Spinner component

---
+++ pages/ui/Spinner.tsx
@@ -0,0 +1,89 @@
+import { COLORS, Div, Spinner } from "cherag-ui";
+import { ScrollView } from "react-native";
+import Layout from "../../components/Layout";
+import { RenderItem } from "../../components/RenderItem";
+import {
+  ProperticsProps,
+  UsecaseArrayObjProps,
+} from "../../components/RenderItem/types";
+import { NextPageWithLayout } from "../_app";
+
+const SpinnerComponent: NextPageWithLayout = () => {
+  let __componentName = "Spinner";
+  let __componentDescription = "Displays a circular loading indicator.";
+  let __importCodeString = `import { Spinner } from 'cherag-ui'`;
+  let __useCaseCodeStringArr: UsecaseArrayObjProps[] = [
+    {
+      name: "",
+      code: `
+    <Spinner color={COLORS.errorIcon} size={"small"} />
+    <Spinner color={COLORS.errorIcon} size={"large"} />
+    <Spinner color={COLORS.errorIcon} size={70} />`,
+    },
+  ];
+  let __propertics: ProperticsProps[] = [
+    {
+      name: "color",
+      type: "string",
+      des: "Color of spinner.",
+    },
+    {
+      name: "size",
+      type: "string | number",
+      des: "Size of spinner.",
+    },
+    {
+      name: "style",
+      type: "StyleProp<ViewStyle>",
+      des: "style of spinner.",
+    },
+  ];
+
+  return (
+    <ScrollView showsHorizontalScrollIndicator={false}>
+      <RenderItem
+        componentName={__componentName}
+        componentDescription={__componentDescription}
+        importCodeString={__importCodeString}
+        usecaseCodeStringArr={__useCaseCodeStringArr}
+        propertics={__propertics}
+      >
+        <Div
+          style={{
+            flexDirection: "row",
+          }}
+        >
+          <Spinner
+            style={{ marginHorizontal: 10 }}
+            color={COLORS.errorIcon}
+            size={"small"}
+          />
+          <Spinner
+            style={{ marginHorizontal: 10 }}
+            color={COLORS.successIcon}
+            size={"large"}
+          />
+          <Spinner
+            style={{ marginHorizontal: 10 }}
+            color={COLORS.errorIcon}
+            size={70}
+          />
+          <Spinner
+            style={{ marginHorizontal: 10 }}
+            color={COLORS.successIcon}
+            size={"large"}
+          />
+          <Spinner
+            style={{ marginHorizontal: 10 }}
+            color={COLORS.errorIcon}
+            size={"small"}
+          />
+        </Div>
+      </RenderItem>
+    </ScrollView>
+  );
+};
+
+SpinnerComponent.getLayout = (page) => <Layout>{page}</Layout>;
+
+export default SpinnerComponent;
