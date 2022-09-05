aca4f9a99f6991e9624b18a8e97926da513c56de
Alauddin Ahmed <alauddin12340@gmail.com>
2022-09-05 20:56:38 +0600

    refactor: update Text component props, code example, example component

--- pages/ui/Text.tsx
+++ pages/ui/Text.tsx
@@ -1,4 +1,4 @@
-import { Button, COLORS, Text } from "cherag-ui";
+import { Button, COLORS, Div, Text } from "cherag-ui";
 import { ScrollView } from "react-native";
 import Layout from "../../components/Layout";
 import { RenderItem } from "../../components/RenderItem";
@@ -12,17 +12,40 @@ const TextComponent: NextPageWithLayout = () => {
   let __componentName = "Text";
   let __componentDescription =
     "A component for displaying text, supports nesting, styling.";
-  let __importCodeString = `import { Text } from 'cherag-ui'`;
+  let __importCodeString = `import { Text, fontSizeProps } from 'cherag-ui'`;
   let __useCaseCodeStringArr: UsecaseArrayObjProps[] = [
     {
-      name: "",
-      code: `
-    <Text style={{ color: COLORS.red }} fontSize="3xl">
+      name: "Example 1",
+      code: `   <Text style={{ color: COLORS.red }} fontSize="3xl">
+      Cherag-ui
+    </Text>`,
+    },
+    {
+      name: "Example 2",
+      code: `    <Text style={{ color: COLORS.green }} fontSize="2xl">
+      Cherag-ui
+    </Text>`,
+    },
+    {
+      name: "Example 3",
+      code: `    <Text style={{ color: COLORS.blue }} fontSize="4xl">
+      Cherag-ui
+    </Text>`,
+    },
+    {
+      name: "Example 4",
+      code: `    <Text style={{ color: COLORS.black }} fontSize="lg">
+      Cherag-ui
+    </Text>`,
+    },
+    {
+      name: "Example 5",
+      code: `    <Text style={{ color: COLORS.sayn }} fontSize="sm">
       Cherag-ui
     </Text>`,
     },
   ];
-  let __propertics: ProperticsProps[] = [
+  let __properticsOne: ProperticsProps[] = [
     {
       name: "children",
       type: "React.ReactNode",
@@ -35,8 +58,8 @@ const TextComponent: NextPageWithLayout = () => {
     },
     {
       name: "fontSize",
-      type: "string",
-      des: "'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl'",
+      type: "fontSizeProps",
+      des: "sm | md | lg | xl | 2xl | 3xl | 4xl",
     },
   ];
 
@@ -47,11 +70,38 @@ const TextComponent: NextPageWithLayout = () => {
         componentDescription={__componentDescription}
         importCodeString={__importCodeString}
         usecaseCodeStringArr={__useCaseCodeStringArr}
-        propertics={__propertics}
+        properticsOneTitle="Text Propertics"
+        properticsOne={__properticsOne}
       >
-        <Text style={{ color: COLORS.red }} fontSize="3xl">
-          Cherag-ui
-        </Text>
+        <Div
+          style={{
+            flex: 1,
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: COLORS.red }} fontSize="3xl">
+            Cherag-ui
+          </Text>
+          <Text style={{ color: COLORS.black }} fontSize="2xl">
+            Cherag-ui
+          </Text>
+          <Text style={{ color: COLORS.lightGray2 }} fontSize="lg">
+            Cherag-ui
+          </Text>
+          <Text style={{ color: COLORS.sayn }} fontSize="sm">
+            Cherag-ui
+          </Text>
+          <Text style={{ color: COLORS.yellow }} fontSize="md">
+            Cherag-ui
+          </Text>
+          <Text style={{ color: COLORS.success }} fontSize="xl">
+            Cherag-ui
+          </Text>
+          <Text style={{ color: COLORS.blue }} fontSize="4xl">
+            Cherag-ui
+          </Text>
+        </Div>
       </RenderItem>
     </ScrollView>
   );
